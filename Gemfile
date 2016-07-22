1fe04656c92f7942a04052deb2c2e01a39aff559
Arthur Goodhart <arthurgoodhart@Arthurs-MacBook-Pro.local>
2016-07-22 10:46:11 +0100

    google callender

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'font-awesome-sass'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'google-api-client', '~> 0.9', require: 'google/apis/calendar_v3'
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,4 +2,17 @@ class CalendarController < ApplicationController
   def show
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
+
+  def redirect
+  client = Signet::OAuth2::Client.new({
+    client_id: ENV.fetch('727945567550-8l8armoaq8ngit07ge25l0m734mctd6k.apps.googleusercontent.com'),
+    client_secret: ENV.fetch('cMRQwADD5vew3BjrehmkYNev'),
+    authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+    scope: Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY,
+    redirect_uri: url_for(:action => :callback)
+  })
+
+  redirect_to client.authorization_uri.to_s
+  end
+
 end
